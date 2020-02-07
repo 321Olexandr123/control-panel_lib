1cc7e9e544538618f4083a22919ee66aaf042dc5
Александр Громский <rskrydge@itlab-studio.com>
2020-02-07 11:31:39 +0200

    mege

--- ControlPanel/Account.php
+++ ControlPanel/Account.php
@@ -28,7 +28,7 @@ class Account
     {
         $client = new NativeHttpClient();
 
-        $response = $client->request('POST', 'https://controlpanel.crpt.trading/project-settings', [
+        $response = $client->request('POST', 'https://controlpanel.crpt.trading/project-settings/provider-crypto/huobi', [
             'headers' => [
                 'Content-Type' => 'application/json',
             ],
